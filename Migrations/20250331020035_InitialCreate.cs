2239b49ea734c0f44a7352d7b6fc3f6a845eae25
Christina Vieau <cvieau@live.com>
2025-03-30 23:36:10 -0400

    addedEndpoints

---
+++ Migrations/20250331020035_InitialCreate.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Todo_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDoItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Task = table.Column<string>(type: "text", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "boolean", nullable: false),
+                    UserProfileId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ToDoItems_UserProfiles_UserProfileId",
+                        column: x => x.UserProfileId,
+                        principalTable: "UserProfiles",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "ToDoItems",
+                columns: new[] { "Id", "IsCompleted", "Task", "UserProfileId" },
+                values: new object[,]
+                {
+                    { 101, true, "Make Grocery List", null },
+                    { 102, false, "Buy groceries", null },
+                    { 103, false, "Finish C# project", null },
+                    { 104, true, "Call mom", null },
+                    { 105, false, "Schedule dentist appointment", null },
+                    { 106, true, "Read 10 pages of a book", null },
+                    { 107, false, "Go for a 30-minute walk", null },
+                    { 108, false, "Reply to emails", null },
+                    { 109, false, "Plan weekend trip", null },
+                    { 110, true, "Organize workspace", null },
+                    { 111, false, "Meal prep for the week", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserProfiles",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 201, "Penny" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDoItems_UserProfileId",
+                table: "ToDoItems",
+                column: "UserProfileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoItems");
+
+            migrationBuilder.DropTable(
+                name: "UserProfiles");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.Json;
 using Todo_App;
+using System.Runtime.CompilerServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,5 +34,78 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/toDoItems", (Todo_AppDbContext db) =>
+{
+    return db.ToDoItems.ToList();
+});
+
+app.MapGet("/toDoItems/{id}", (Todo_AppDbContext db, int id) =>
+{
+    return db.ToDoItems.Single(t => t.Id == id);
+});
+
+app.MapPost("/toDoItems", (Todo_AppDbContext db, ToDoItem toDoItem) =>
+{
+    db.ToDoItems.Add(toDoItem);
+    db.SaveChanges();
+    return Results.Created($"/toDoItems/{toDoItem.Id}", toDoItem);
+});
+
+app.MapDelete("/toDoItems/{id}", (Todo_AppDbContext db, int id) =>
+{
+    ToDoItem toDoItem = db.ToDoItems.SingleOrDefault(toDoItem => toDoItem.Id == id);
+    if (toDoItem == null)
+    {
+        return Results.NotFound();
+    }
+    db.ToDoItems.Remove(toDoItem);
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+app.MapPut("/toDoItems/{id}", (Todo_AppDbContext db, int id, ToDoItem toDoItem) =>
+{
+    ToDoItem toDoItemToUpdate = db.ToDoItems.SingleOrDefault(toDoItem => toDoItem.Id == id);
+    if (toDoItemToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    toDoItemToUpdate.Task = toDoItem.Task;
+    toDoItemToUpdate.IsCompleted = toDoItem.IsCompleted;
+    
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+app.MapGet("/userProfiles", (Todo_AppDbContext db) =>
+{
+    return db.UserProfiles.ToList();
+});
+
+app.MapGet("/userProfiles/{id}", (Todo_AppDbContext db, int id) =>
+{
+    return db.UserProfiles.Single(u => u.Id == id);
+});
+
+app.MapPost("/userProfiles", (Todo_AppDbContext db, UserProfile userProfile) =>
+{
+    db.UserProfiles.Add(userProfile);
+    db.SaveChanges();
+    return Results.Created($"/toDoItems/{userProfile.Id}", userProfile);
+});
+
+app.MapPut("/userProfiles/{id}", (Todo_AppDbContext db, int id, UserProfile userProfile) =>
+{
+    UserProfile userProfileToUpdate = db.UserProfiles.SingleOrDefault(userProfile => userProfile.Id == id);
+    if (userProfileToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    userProfileToUpdate.Name = userProfile.Name;
+
+    db.SaveChanges();
+    return Results.NoContent();
+});
 
 app.Run();
