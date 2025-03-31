2f5979271930272bfc9db57673ef33770457b60e
Christina Vieau <cvieau@live.com>
2025-03-30 20:55:51 -0400

    added UserProfile and ToDoItem models

---
+++ Models/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo_App.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        public List<ToDoItem> ToDoItems { get; set; } = new List<ToDoItem>();
+    }
+}
